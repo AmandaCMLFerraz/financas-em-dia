c3263823233efdb3247071791cb91fb772cc73c0
AmandaCMLFerraz <amandaferraz@unochapeco.edu.br>
2025-05-26 19:56:39 -0300

    :construction: Ajustando o prisma ORM

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,3 +1,14 @@
 import fastify from "fastify";
+import { PrismaClient } from "../src/generated/prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: 'Amanda Ferraz',
+        email: 'amandacferraz@hotmail.com',
+        password: 'Amand@1806',
+    },
+})
