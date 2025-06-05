53e0d099e33efdfdf525f638bdccce0428aba65d
AmandaCMLFerraz <amandaferraz@unochapeco.edu.br>
2025-06-04 22:03:27 -0300

    🚧 Tela de Login

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
 import './App.css'
-import { Register } from './pages/register/register'
+import { Register } from './pages/register/Register.tsx'
+import { Login } from './pages/login/Login.tsx'
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Register />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- front-end/src/pages/login/Login.tsx
+++ front-end/src/pages/login/Login.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
-import styles from './Register.module.css'
+import styles from './Login.module.css'
+import imagem from '../../assets/image.png'
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import AlternateEmailRoundedIcon from '@mui/icons-material/AlternateEmailRounded';
+
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import axios from 'axios';  
+import axios from 'axios';
 
 type Props = {
   name: string
@@ -10,43 +14,24 @@ type Props = {
   password: string
 }
 
-export const Register: React.FC<Props> = () => {
+export const Login: React.FC<Props> = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    
-    const data = { name, email, password };
-
-    try {
-      const response = await axios.post('http://localhost:3333/users', data);  
-      console.log('Usuário cadastrado com sucesso:', response.data);
-    } catch (error) {
-      console.error('Erro ao cadastrar usuário:', error);
-    }
-  }
 
   return (
     <div className={styles.container}>
+      <div className={styles.imagem}>
+        <img src={imagem} alt="Imagem de finanças" />
+      </div>
       <div className={styles.form}>
         <h1>Finanças em Dia</h1>
-        <h3>Porque cuidar do seu dinheiro é investir no seu futuro.</h3>
-        
-        <form onSubmit={handleSubmit}>
-          <TextField
-            label="Nome"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            sx={{
-              width: '300px',
-              '& .MuiOutlinedInput-root': {
-                borderRadius: '24px', // Aplica o borderRadius ao input
-              }
-            }}
-          />
-
+        <div>
+          <FacebookRoundedIcon sx={{ color: 'white', fontSize: '40px'}} />
+          <AlternateEmailRoundedIcon sx={{ color: 'white', fontSize: '40px'}} />
+        </div>
+        <form>
           <TextField
             label="E-mail"
             type='email'
@@ -55,7 +40,7 @@ export const Register: React.FC<Props> = () => {
             sx={{
               width: '300px',
               '& .MuiOutlinedInput-root': {
-                borderRadius: '24px', // Aplica o borderRadius ao input
+                borderRadius: '24px',
               }
             }}
           />
@@ -69,7 +54,7 @@ export const Register: React.FC<Props> = () => {
               width: '300px',
               '& .MuiOutlinedInput-root': {
                 borderRadius: '24px', // Aplica o borderRadius ao input
-              }
+              },
             }}
           />
 
@@ -79,12 +64,14 @@ export const Register: React.FC<Props> = () => {
             sx={{
               width: '150px',
               borderRadius: '24px',
-              backgroundColor: '#407cff',
+              backgroundColor: '#fff',
+              color: '#407cff',
+              border: 'none',
               transition: 'transform 0.2s ease', // Transição suave para o efeito de escala
-              '&:hover': { backgroundColor: '#3366CC', transform: 'scale(1.05)' }
+              '&:hover': { transform: 'scale(1.05)' }
             }}
           >
-            Cadastre-se
+            Iniciar sessão
           </Button>
         </form>
       </div>
